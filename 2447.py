61c4e6f7783103d6b6d30f5160f0b7fe015358f5
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2020-08-27 16:26:21 +0900

    Update 2447.py

--- 2447.py
+++ 2447.py
@@ -6,9 +6,10 @@ input = sys.stdin.readline
 N = int(input())
 
 def star(x):
+    print("제발 되라")
     a = 1
     b = 2
     x = a + b
-    return x
+    return 10
 
-print(star(10))
+print("왜 안돼 이거ㅠㅠㅠㅠㅠㅠ")
