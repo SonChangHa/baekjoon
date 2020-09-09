87e3d1fef289c5f33f4a6571a69bedd2b57866d1
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2020-09-09 15:55:16 +0900

    Create 5338.py

---
+++ 5338.py
@@ -0,0 +1,11 @@
+import sys
+def put():
+    return list(map(int, input().split()))
+input = sys.stdin.readline
+
+print('''       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+''')
