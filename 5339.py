79bc02a93f30a1f344f134e7036c41b0b2638bdb
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2020-09-09 15:56:23 +0900

    Create 5339.py

---
+++ 5339.py
@@ -0,0 +1,16 @@
+import sys
+def put():
+    return list(map(int, input().split()))
+input = sys.stdin.readline
+
+print('''     /~\\
+    ( oo|
+    _\=/_
+   /  _  \\
+  //|/.\|\\\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |
+''')
