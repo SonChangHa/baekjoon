c2ba885c1fc0ef89f784a042b34f87cc0b71abf3
SonChangHa <jjang00198@gmail.com>
2021-10-19 21:37:22 +0900

    Create 11286.py

---
+++ 11286.py
@@ -0,0 +1,27 @@
+import heapq
+import sys
+
+
+def put():
+    return list(map(int, input().split()))
+
+
+input = sys.stdin.readline
+
+
+heap = []
+
+
+for i in range(int(input())):
+    t = int(input())
+    if t != 0:
+        if t > 0:
+            heapq.heappush(heap, (t, t))
+        else:
+            heapq.heappush(heap, (-t, t))
+
+    else:
+        if not heap:
+            print("0")
+        else:
+            print(heapq.heappop(heap)[1])
