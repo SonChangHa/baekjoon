0742e579b910da6e31522efb544bb42a49fbe6cc
SonChangHa <jjang00198@gmail.com>
2022-04-18 22:19:49 +0900

    done

--- 9095.py
+++ 9095.py
@@ -6,9 +6,9 @@ for _ in range(t):
 
     n = int(input())
 
-    dp = [0] * n # [1, 2, 3, 5]
-    dp[0] = 1 # 손으로 계산해서 2x1일때 경우 계산
-    dp[1] = 2 # 마찬가지로 2x2일때 경우 계산
+    dp = [0] * n
+    dp[0] = 1
+    dp[1] = 2
     dp[2] = 4
 
     for i in range(3, n):
